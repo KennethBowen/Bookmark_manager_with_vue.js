738c761053f7fe0845722d6ae63ab3b991529576
Kenneth Bowen <bowenk479@gmail.com>
2016-06-17 19:19:18 -0700

    set nokogiri and httparty to retrieve a url and create a document from it

--- app/controllers/crawl_controller.rb
+++ app/controllers/crawl_controller.rb
@@ -1,6 +1,17 @@
 class CrawlController < ApplicationController
   def index
     url = params[:url]
+    document = Nokogiri::HTML(HTTParty.get(url))
     
+    #use css method to base content off css selector
+    title = document.css("title").first.text
+    image = document.css("img").first.attr("src") rescue nil
+    paragraph = document.css("p").first.text rescue nil
+
+    render json: {
+    title: title,
+    image: image,
+    paragraph: paragraph
+  }
   end
 end
