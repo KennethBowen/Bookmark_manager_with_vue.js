1c59bbfa9f453aa142ec4dcf453f5686837e6f2d
Kenneth Bowen <bowenk479@gmail.com>
2016-06-17 18:13:37 -0700

    migrated slug_to_bookmarks

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160618003340) do
+ActiveRecord::Schema.define(version: 20160618011216) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.string   "url"
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "slug"
   end
 
 end
