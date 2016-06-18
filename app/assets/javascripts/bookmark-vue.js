0dc9afa0bdb6ec5d08e4385f3355afd8715a79a2
Kenneth Bowen <bowenk479@gmail.com>
2016-06-17 19:42:15 -0700

    added new_bookmark element and created the function loadBookmarkVue for turblinks

--- app/assets/javascripts/bookmark-vue.js
+++ app/assets/javascripts/bookmark-vue.js
@@ -1,28 +1,30 @@
-new Vue({
-  el: "#"
-  data: {
-    title, null,
-    url: null,
-    error: null
-  },
-
-  methods: {
-    urlChanged: function() {
-      loadPage();
+function loadBookmarkVue() {
+  new Vue({
+    el: "#new_bookmark"
+    data: {
+      title, null,
+      url: null,
+      error: null
     },
 
-    loadPage: function() {
-      var _this = this;
-      $.ajax({
-        url: window.loacation.origin + "/crawl/" + _this.url, //http://localhost:3000/crawl/http://the_url_trying_to_pass
-        contentType: "application/json"
-        dataType: "json"
-      }).done(function(data) {
-        _this.title = data.title;
-        _this.error = null;
-      }).fail(function(data) {
-        _this.error = "something went wrong."
-      }) ;
+    methods: {
+      urlChanged: function() {
+        loadPage();
+      },
+
+      loadPage: function() {
+        var _this = this;
+        $.ajax({
+          url: window.loacation.origin + "/crawl/" + _this.url, //http://localhost:3000/crawl/http://the_url_trying_to_pass
+          contentType: "application/json"
+          dataType: "json"
+        }).done(function(data) {
+          _this.title = data.title;
+          _this.error = null;
+        }).fail(function(data) {
+          _this.error = "something went wrong."
+        }) ;
+      }
     }
-  }
-});
+  });
+}
